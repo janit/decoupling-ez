69c031671aae4de7969cfdfc1a5abe76e59021db
Jani Tarvainen <jani.tarvainen@ez.no>
2016-08-29 10:11:07 +0300

    Adding cache clear

--- installation/reset.sh
+++ installation/reset.sh
@@ -2,3 +2,4 @@ mysql -usummer -pcamp -e "TRUNCATE TABLE kaliop_migrations;" headless
 php app/console cache:clear
 php app/console ezplatform:install clean
 php app/console kaliop:migration:migrate -n
+php app/console cache:clear --env=prod

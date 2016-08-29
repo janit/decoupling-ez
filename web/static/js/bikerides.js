0a6126e2813a06b3ba6f85b9dce39e7ee97459fa
Jani Tarvainen <jani.tarvainen@ez.no>
2016-08-29 10:24:47 +0300

    Work

---
+++ web/static/js/bikerides.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#app',
+    data: {
+        rides: [
+            {
+                'image': 'images/toulousse.png',
+                'title': 'title 1',
+                'author': 'da author',
+                'route': 'Lyon Toulousse',
+                'distance': '30',
+                'level': 'easy'
+            },
+            {
+                'image': 'images/toulousse.png',
+                'title': 'title 2',
+                'author': 'da author',
+                'route': 'Lyon Toulousse',
+                'distance': '30',
+                'level': 'easy'
+            },
+            {
+                'image': 'images/toulousse.png',
+                'title': 'title 3',
+                'author': 'da author',
+                'route': 'Lyon Toulousse',
+                'distance': '30',
+                'level': 'easy'
+            },
+        ]
+    }
+})

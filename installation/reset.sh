5092e07151c0b6ff50170307a938161b9f5c1aab
Jani Tarvainen <jani.tarvainen@ez.no>
2016-09-01 07:44:42 +0200

    adding tags pull

--- installation/reset.sh
+++ installation/reset.sh
@@ -1,5 +1,6 @@
 mysql -usummer -pcamp -e "TRUNCATE TABLE kaliop_migrations;" headless
 composer install
+git pull --tags
 php app/console cache:clear
 php app/console ezplatform:install clean
 php app/console kaliop:migration:migrate -n

cd38c85f8d941e339867835f5fd8214f14d39e64
Jani Tarvainen <jani.tarvainen@ez.no>
2016-09-01 08:33:20 +0200

    Bike rides to be static still

--- web/static/js/bikerides.js
+++ web/static/js/bikerides.js
@@ -3,6 +3,32 @@ new Vue({
   data: {
     message: '',
     searchWord: '',
+    rides: [
+        {
+            'image': 'images/toulousse.png',
+            'title': 'title 1',
+            'author': 'da author',
+            'route': 'Lyon Toulousse',
+            'distance': '30',
+            'level': 'easy'
+        },
+        {
+            'image': 'images/toulousse.png',
+            'title': 'title 2',
+            'author': 'da author',
+            'route': 'Lyon Toulousse',
+            'distance': '30',
+            'level': 'easy'
+        },
+        {
+            'image': 'images/toulousse.png',
+            'title': 'title 3',
+            'author': 'da author',
+            'route': 'Lyon Toulousse',
+            'distance': '30',
+            'level': 'easy'
+        },
+    ],    
     rides: [],
     capi: false,
     contentService: false
@@ -42,68 +68,6 @@ new Vue({
     refreshBikerideList: function(){
 
         console.log('do search');
-
-        var that = this;
-
-        query = this.contentService.newViewCreateStruct('bikesrides-view', 'ContentQuery');
-
-
-        query.body.ViewInput.ContentQuery.Criteria = {
-            ContentTypeIdentifierCriterion: 'ride'
-        };
-
-        query.body.ViewInput.ContentQuery.limit = 10;
-
-        // query.body.ViewInput.LocationQuery.offset = 0;
-        this.contentService.createView(query, function (error, response) {
-            if ( error ) {
-                console.log('Error!');
-                return;
-            }
-            
-            var bikeRides = [];
-            var searchHits = response.document.View.Result.searchHits.searchHit;
-
-            for (i in searchHits){
-
-                var bikeRide = {};
-
-                var fields = searchHits[i].value.Content.CurrentVersion.Version.Fields.field;
-
-                for(j in fields){
-                    bikeRide[fields[j].fieldDefinitionIdentifier] = that.getSimpleValue(fields[j]);
-                }
-
-                bikeRides.push(bikeRide);
-
-            }
-
-            that.rides = bikeRides;
-
-        });
-    
-    },
-
-    getSimpleValue: function(field){
-
-        if(field.fieldDefinitionIdentifier == "description"){
-            return field.fieldValue.xhtml5edit;
-
-        } else if(field.fieldDefinitionIdentifier == "image"){
-            return field.fieldValue.uri;
-
-        } else if(field.fieldDefinitionIdentifier == "difficulty_level"){
-
-            if(field.fieldValue.length > 0){
-                var levels = ['easy','intermediate','hard'];
-                return levels[field.fieldValue[0]];
-
-            }
-
-            return 'not set';
-        }
-
-        return field.fieldValue;
 
     }
 
